fc4fa6bd030a52dbe48b2b1c5980389d097db911
paul <paulentz@gmail.com>
2023-02-26 20:00:20 -0600

    did stuff

--- practice/externalPractice.js
+++ practice/externalPractice.js
@@ -1,1 +1,7 @@
 console.log("Hello from outside page");
+console.log("It's a me, jario");
+console.log("the page title:", document.title);
+console.log("the protocol:", window.location.protocol);
+let p = document.getElementById("meat");
+p.append("..and I hope you're doing well and having a good day. Screeee");
+console.log(p.innerHTML);
